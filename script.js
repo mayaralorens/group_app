3f3b6d2714c0cd2b22b79220922b9a5d94b250dc
thayenelorens <thayenelorens@gmail.com>
2022-04-27 16:33:15 +0100

    Js and CSS files ready

--- script.js
+++ script.js
@@ -1,96 +1,94 @@
-// Titles: https://omdbapi.com/?s=thor&page=1&apikey=fc1fef96
-// details: http://www.omdbapi.com/?i=tt3896198&apikey=fc1fef96
-
 //My key: http://www.omdbapi.com/?i=tt3896198&apikey=354ca4f9
 
-const movieSearchBox = document.getElementById('movie-search-box');
-const searchList = document.getElementById('search-list');
-const resultGrid = document.getElementById('result-grid');
+const movieSearchBar = document.getElementById('find-movie-here');
+const contentList = document.getElementById('search-list');
+const results = document.getElementById('output');
 
 // load movies from API
-async function loadMovies(searchTerm){
-    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
+async function uploadMovies(searchTerm){
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=354ca4f9`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
     // console.log(data.Search);
-    if(data.Response == "True") displayMovieList(data.Search);
+    if(data.Response == "True") displayInfo(data.Search);
 }
 
-function findMovies(){
-    let searchTerm = (movieSearchBox.value).trim();
+function trackMovies(){
+    let searchTerm = (movieSearchBar.value).trim();
     if(searchTerm.length > 0){
-        searchList.classList.remove('hide-search-list');
-        loadMovies(searchTerm);
+        contentList.classList.remove('hide-search-list');
+        uploadMovies(searchTerm);
     } else {
-        searchList.classList.add('hide-search-list');
+        contentList.classList.add('hide-search-list');
     }
 }
 
-function displayMovieList(movies){
-    searchList.innerHTML = "";
-    for(let idx = 0; idx < movies.length; idx++){
-        let movieListItem = document.createElement('div');
-        movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
-        movieListItem.classList.add('search-list-item');
-        if(movies[idx].Poster != "N/A")
-            moviePoster = movies[idx].Poster;
+function displayInfo(movies){
+    contentList.innerHTML = "";
+    for(let i = 0; i < movies.length; i++){
+        let movieSearchList = document.createElement('div');
+        movieSearchList.dataset.id = movies[i].imdbID; // setting movie id in  data-id
+        movieSearchList.classList.add('search-list-item');
+        if(movies[i].Poster != "N/A")
+            moviePoster = movies[i].Poster;
         else 
-            moviePoster = "image_not_found.png";
+            moviePoster = "image-not-found.jpg";
 
-        movieListItem.innerHTML = `
+        movieSearchList.innerHTML = `
         <div class = "search-item-thumbnail">
             <img src = "${moviePoster}">
         </div>
         <div class = "search-item-info">
-            <h3>${movies[idx].Title}</h3>
-            <p>${movies[idx].Year}</p>
+            <h3>${movies[i].Title}</h3>
+            <p>${movies[i].Year}</p>
         </div>
         `;
-        searchList.appendChild(movieListItem);
+        contentList.appendChild(movieSearchList);
     }
     loadMovieDetails();
 }
 
 function loadMovieDetails(){
-    const searchListMovies = searchList.querySelectorAll('.search-list-item');
-    searchListMovies.forEach(movie => {
+    const movieFinder = contentList.querySelectorAll('.search-list-item');
+    movieFinder.forEach(movie => {
         movie.addEventListener('click', async () => {
             // console.log(movie.dataset.id);
-            searchList.classList.add('hide-search-list');
-            movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fc1fef96`);
+            contentList.classList.add('hide-search-list');
+            movieSearchBar.value = "";
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=354ca4f9`);
             const movieDetails = await result.json();
             // console.log(movieDetails);
-            displayMovieDetails(movieDetails);
+            displayMovieInput(movieDetails);
         });
     });
 }
 
-function displayMovieDetails(details){
-    resultGrid.innerHTML = `
-    <div class = "movie-poster">
-        <img src = "${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt = "movie poster">
+function displayMovieInput(info){
+    results.innerHTML = `
+    <div class = "movie-cover">
+        <img src = "${(info.Poster != "N/A") ? info.Poster : "image-not-found.jpg"}" alt = "movie poster">
     </div>
-    <div class = "movie-info">
-        <h3 class = "movie-title">${details.Title}</h3>
-        <ul class = "movie-misc-info">
-            <li class = "year">Year: ${details.Year}</li>
-            <li class = "rated">Ratings: ${details.Rated}</li>
-            <li class = "released">Released: ${details.Released}</li>
+    <div class = "movie-description">
+        <h3 class = "movie-title">${info.Title}</h3>
+        <ul class = "movie-release-date">
+            <li class = "year">Year: ${info.Year}</li>
+            <li class = "rates">Ratings: ${info.Rated}</li>
+            <li class = "release">Released: ${info.Released}</li>
         </ul>
-        <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
-        <p class = "writer"><b>Writer:</b> ${details.Writer}</p>
-        <p class = "actors"><b>Actors: </b>${details.Actors}</p>
-        <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
-        <p class = "language"><b>Language:</b> ${details.Language}</p>
-        <p class = "awards"><b><i class = "fas fa-award"></i></b> ${details.Awards}</p>
+        <p class = "genre"><strong>Genre:</strong> ${info.Genre}</p>
+        <p class = "type"><strong>Type:</strong> ${info.Type}</p>
+        <p class = "writer"><strong>Writer:</strong> ${info.Writer}</p>
+        <p class = "actors"><strong>Actors: </strong>${info.Actors}</p>
+        <p class = "plot"><strong>Plot:</strong> ${info.Plot}</p>
+        <p class = "language"><strong>Language:</strong> ${info.Language}</p>
+        <p class = "awards"><strong><i class = "fas fa-award"></i></strong> ${info.Awards}</p>
     </div>
     `;
 }
 
 
 window.addEventListener('click', (event) => {
-    if(event.target.className != "form-control"){
-        searchList.classList.add('hide-search-list');
+    if(event.target.className != "form"){
+        contentList.classList.add('hide-search-list');
     }
 });
