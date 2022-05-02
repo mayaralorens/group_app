614f847c85607c728188c2437b1085dca74da150
thayenelorens <thayenelorens@gmail.com>
2022-05-03 00:04:45 +0100

    Commenting main.js file

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
+//We created this file based on the classes and on this youtube video: https://www.youtube.com/watch?v=kN1Czs0m1SU
+
+//electron variable will require electron
 const electron = require('electron');
+//require the path module
 const path = require('path');
+//require url, which is a core js module
 const url = require('url');
 
-const {app, BrowserWindow, Menu, ipcMain} = electron;
+//Pulling these objects out of electron
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
-// Listen for app to be ready
-app.on('ready', function(){
-  // Create new window
+//Listen for app to be ready
+app.on('ready', function () {
+  //Create new window once the app is ready. 
   mainWindow = new BrowserWindow({});
-  // Load html in window
+  //Load the html file "index.html" into the mainWindow.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
-    slashes:true
+    slashes: true
   }));
   // Quit app when closed
-  mainWindow.on('closed', function(){
+  mainWindow.on('closed', function () {
     app.quit();
   });
 
@@ -27,79 +33,72 @@ app.on('ready', function(){
   Menu.setApplicationMenu(mainMenu);
 });
 
-// Handle search item window
-function createSearchWindow(){
+// Handle createSearch window
+function createSearchWindow() {
   searchWindow = new BrowserWindow({
+    //Setting widt, height and a title for this window.
     width: 500,
-    height:500,
-    title:'Insert Movie Title'
+    height: 500,
+    title: 'Insert Movie Title'
   });
+  //Load the html file "movies.html" into the searchWindow.
   searchWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'movies.html'),
     protocol: 'file:',
-    slashes:true
+    slashes: true
   }));
-  // Handle garbage collection
-  searchWindow.on('close', function(){
+  // Garbage collection handle
+  searchWindow.on('close', function () {
     searchWindow = null;
   });
 }
 
-// Handle about item window
-function createAboutWindow(){
+// Handle createAbout window
+function createAboutWindow() {
   aboutWindow = new BrowserWindow({
+    //Setting widt, height and a title for this window.
     width: 500,
-    height:500,
-    title:'About movieHunters'
+    height: 500,
+    title: 'About movieHunters'
   });
   aboutWindow.loadURL(url.format({
+    //Load the html file "about.html" into the searchWindow.
     pathname: path.join(__dirname, 'about.html'),
     protocol: 'file:',
-    slashes:true
+    slashes: true
   }));
   // Handle garbage collection
-  aboutWindow.on('close', function(){
+  aboutWindow.on('close', function () {
     aboutWindow = null;
   });
 }
 
-// Catch item:search
-ipcMain.on('item:search', function(e, item){
-  mainWindow.webContents.send('item:search', item);
-  searchWindow.close(); 
-  // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
-  //addWindow = null;
-});
-
-// Catch item:about
-ipcMain.on('item:about', function(e, item){
-  mainWindow.webContents.send('item:about', item);
-  aboutWindow.close(); 
-  // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
-  //addWindow = null;
-});
 // Create menu template
-const mainMenuTemplate =  [
+const mainMenuTemplate = [
   // Each object is a dropdown
   {
     label: 'File',
-    submenu:[
+    submenu: [
       {
-        label:'Search movies',
-        click(){
+        //Search movies label
+        label: 'Search movies',
+        click() {
           createSearchWindow();
         }
       },
       {
-        label:'About MovieHunters',
-        click(){
-         createAboutWindow();
+        //About MovieHunters label
+        label: 'About MovieHunters',
+        click() {
+          createAboutWindow();
         }
       },
       {
+        //Quit label
         label: 'Quit',
-        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-        click(){
+        //shortcut to quit by clicking on ctrl + Q in the keyboard.
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
           app.quit();
         }
       }
@@ -107,23 +106,23 @@ const mainMenuTemplate =  [
   }
 ];
 
-// If OSX, add empty object to menu
-if(process.platform == 'darwin'){
+// If mac, add empty object to menu
+if (process.platform == 'darwin') {
   mainMenuTemplate.unshift({});
 }
 
-// Add developer tools option if in dev
-if(process.env.NODE_ENV !== 'production'){
+// Add developer tools option 
+if (process.env.NODE_ENV !== 'production') {
   mainMenuTemplate.push({
     label: 'Developer Tools',
-    submenu:[
+    submenu: [
       {
         role: 'reload'
       },
       {
         label: 'Toggle DevTools',
-        accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
-        click(item, focusedWindow){
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow) {
           focusedWindow.toggleDevTools();
         }
       }
